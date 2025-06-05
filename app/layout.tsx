b0d432a1a36f97fbd063b3c6194621f530d50fb8
wabrocker <billbrocker@gmail.com>
2025-06-05 11:36:46 -0400

    implement light & dark themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -29,11 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 const Home = () => (
   <>
-    <h1 className="h1-bold">Welcome to Next.js</h1>;
-    <h1 className="h1-bold font-space-grotesk">Welcome to Next.js (Space)</h1>;
+    <h1 className="h1-bold">Welcome to Next.js</h1>
   </>
 );
 
